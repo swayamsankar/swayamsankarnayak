6cccd9a3ff4aead8901f68771ec2745f19614654
swayamsankar <swayamsankar259@gmail.com>
2026-07-23 11:43:38 +0530

    Fix sitemap base URL

--- src/routes/sitemap[.]xml.ts
+++ src/routes/sitemap[.]xml.ts
@@ -2,7 +2,7 @@ import { createFileRoute } from "@tanstack/react-router";
 import type {} from "@tanstack/react-start";
 
 // TODO: replace with your project URL once a project name or custom domain is set.
-const BASE_URL = "";
+const BASE_URL = "https://swayamsankarnayak.swayamsankar259.workers.dev";
 
 interface SitemapEntry {
   path: string;
